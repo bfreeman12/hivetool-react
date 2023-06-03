f052167d4cfbe92dba4d289acd83e57ac3179ceb
Roy Storey <imlejiii@gmail.com>
2023-06-03 14:53:35 +0000

    fixed and cleaned

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./assets/global.css";
+import React, { useState } from "react";
+import Papa from "papaparse";
 
 function App() {
   //TODO: TAKE ALL INPUT AND DISPLAY CORRECTLY IN THE OUTPUT TEXTAREA
@@ -10,46 +12,130 @@ function App() {
   //TODO: PARSE UPLOADED CSV FILES
   //TODO: SHA256 HASH FILES AND RETURN THE HASHES TO TEXT AREA
 
+  const [currentCsvData, setCurrentCsvData] = useState([{}]);
+
+  const csvUploadHandler = (event) => {
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        setCurrentCsvData(results.data);
+        console.log(results.data);
+      },
+    });
+  };
+
+  var currentdate = new Date();
+  var datetime =
+    currentdate.getMonth() +
+    1 +
+    "/" +
+    currentdate.getDate() +
+    "/" +
+    currentdate.getFullYear() +
+    " @ " +
+    currentdate.getHours() +
+    ":" +
+    currentdate.getMinutes() +
+    "Z";
+
+  const [operatorInitials, setOperatorInitials] = useState("");
+  const [mitreVectors, setMitreVectors] = useState("");
+  const [suricataAlerts, setSuricataAlerts] = useState("");
+  const [description, setDescription] = useState("");
+  const [recommendedRemediation, setRecommendedRemediation] = useState("");
+
+  const [outputText, setOutputText] = useState("No Data Entered");
+
+  function handleFormatOnClick() {
+    var uniqueSrcIPs = [];
+    var uniqueSrcPorts = [];
+    var uniqueDstIPs = [];
+    var uniqueDstPorts = [];
+    var uniqueCommunityIds = [];
+
+    setOperatorInitials(document.getElementById("initials-input").value);
+    setMitreVectors(document.getElementById("attack-vector-input").value);
+    setSuricataAlerts(document.getElementById("alert-input").value);
+    setDescription(document.getElementById("description-input").value);
+    setRecommendedRemediation(
+      document.getElementById("remediation-input").value
+    );
+    currentCsvData.forEach((entry) => {
+      if (!uniqueSrcIPs.includes(" " + entry["Src IP"])) {
+        uniqueSrcIPs.push(" " + entry["Src IP"]);
+      }
+      if (!uniqueSrcPorts.includes(" " + entry[" Src Port"])) {
+        uniqueSrcPorts.push(" " + entry[" Src Port"]);
+      }
+      if (!uniqueDstIPs.includes(" " + entry[" Dst IP"])) {
+        uniqueDstIPs.push(" " + entry[" Dst IP"]);
+      }
+      if (!uniqueDstPorts.includes(" " + entry[" Dst Port"])) {
+        uniqueDstPorts.push(" " + entry[" Dst Port"]);
+      }
+      if (!uniqueCommunityIds.includes("\n" + entry[" Community Id"])) {
+        uniqueCommunityIds.push("\n" + entry[" Community Id"]);
+      }
+    });
+
+    setOutputText(
+      "**Time Observed:** " +
+        datetime +
+        " by: " +
+        operatorInitials +
+        "\n\n**Src IP:**  " +
+        uniqueSrcIPs +
+        "\n\n**Src Ports:**  " +
+        uniqueSrcPorts +
+        "\n\n**Dst IP:**  " +
+        uniqueDstIPs +
+        "\n\n**Dst Ports:**  " +
+        uniqueDstPorts +
+        "\n\n**Community IDs:**\n" +
+        uniqueCommunityIds +
+        "\n\n**Observable Hashes:**\n\n**MITRE Vectors of Attack:**\n\n" +
+        mitreVectors +
+        "\n\n**Suricata Alerts:**\n\n" +
+        suricataAlerts +
+        "\n\n**Description:**\n\n" +
+        description +
+        "\n\n**Recommended Remediation:**\n\n" +
+        recommendedRemediation
+    );
+  }
   function toggleInstructionsModal() {
-    document.getElementById('instructions-page').classList.toggle('visible')
+    document.getElementById("instructions-page").classList.toggle("visible");
   }
 
-  function formatOperatorInitials() {
-    let operatorInitials = document.getElementById('initials-input').value
-    return operatorInitials
-  }
-  function formatMitreVectors() {
-    let mitreVectors = document.getElementById('attack-vector-input').value
-    return mitreVectors
-  }
-  function formatSuricataAlerts() {
-    let suricataAlerts = document.getElementById('alert-input').value
-    return suricataAlerts
-  }
-  function formatDescription() {
-    let description = document.getElementById('description-input').value
-    return description
-  }
-  function formatRecommendedRemediation() {
-    let recommendedRemediation = document.getElementById('remediation-input').value
-    return recommendedRemediation
-  }
-
-  function formatInputtedData(operatorInitials, mitreVectors, suricataAlerts, description, recommendedRemediation) {
-    let formattedData = operatorInitials + mitreVectors + suricataAlerts + description + recommendedRemediation
-    document.getElementById('formatted-output').innerText = formattedData
-  }
   return (
     <div id="content-container" className="content-container">
-      <div id="instructions-button" className="instructions-button" title="tooltip" onClick={toggleInstructionsModal}>Instructions</div>
+      <div
+        id="instructions-button"
+        className="instructions-button"
+        title="tooltip"
+        onClick={toggleInstructionsModal}
+      >
+        Instructions
+      </div>
       <div id="instructions-page" className="instructions-page">
-        <div id="x-button" className="x-button" onClick={toggleInstructionsModal}>X</div>
+        <div
+          id="x-button"
+          className="x-button"
+          onClick={toggleInstructionsModal}
+        >
+          X
+        </div>
         <h4>STEP 1</h4>
         <p>In arkime, export the .csv for all sessions with columns:</p>
         <p>srcIp,srcPort,dstIp,dstPort,communityId</p>
         <p>Then, drop it in the .csv drop area.</p>
         <h4>STEP 2</h4>
-        <p>Compile all pcaps into a single pcap in arkime, that way you can pull all of the http items and stuff for hashing. This can be done using the arrow in the top right of the Arkime window.</p>
+        <p>
+          Compile all pcaps into a single pcap in arkime, that way you can pull
+          all of the http items and stuff for hashing. This can be done using
+          the arrow in the top right of the Arkime window.
+        </p>
         <p>a. Save all observables from the .pcap</p>
         <p>b. Drop all observables into the observable drop area.</p>
         <h4>STEP 3</h4>
@@ -82,13 +168,15 @@ function App() {
         </div>
         <div className="upload-buttons-wrapper">
           <div className="upload-button">
-            <label className="file-upload-label">
+            <label className="file-upload-label" id="csv-upload-label">
               Click here or Drag a .csv file to upload
               <input
+                name="file"
                 id="csv-upload"
                 type="file"
                 accept=".csv"
                 style={{ display: "none" }}
+                onChange={csvUploadHandler}
               />
             </label>
           </div>
@@ -104,10 +192,12 @@ function App() {
             </label>
           </div>
         </div>
-        <button id="submit-button" onClick={() => formatInputtedData(formatOperatorInitials(), formatMitreVectors(), formatSuricataAlerts(), formatDescription(), formatRecommendedRemediation())}>Format!</button>
+        <button id="submit-button" onClick={handleFormatOnClick}>
+          Format!
+        </button>
       </div>
       <div className="output-container">
-        <div id="formatted-output"></div>
+        <textarea id="formatted-output" value={outputText}></textarea>
       </div>
     </div>
   );
